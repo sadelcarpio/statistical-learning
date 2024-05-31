413b5bb473703b6f1d25b4738a36f7c60a073f17
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-31 10:04:34 -0500

    Documentation

--- knn-classification/include/KnnRegressor.hpp
+++ knn-classification/include/KnnRegressor.hpp
@@ -4,7 +4,7 @@
 class KnnRegressor : public Knn<double> {
 private:
     /**
-     *
+     * Gets the response as an average of K nearest responses.
      * @param distances vector of pairs of (index, distance) values
      * @return The average of the responses (y values) of the K nearest neighbors.
      */
