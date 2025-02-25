efa877cc1745e448908879610739f300ff2e8eb2
sadelcarpio <sadelcarpioa@gmail.com>
2025-02-25 16:37:11 -0500

    better

--- classification/src/Main.java
+++ classification/src/Main.java
@@ -11,19 +11,19 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Metric[] metrics = new Metric[]{new Accuracy()};
-        Dataset dataset = new Dataset("data/qda_classification.csv");
+        Dataset dataset = new Dataset("data/dummy_data_classification.csv");
         System.out.println("Labels: " + Arrays.deepToString(dataset.getLabels()));
         System.out.println("Predictors: " + Arrays.deepToString(dataset.getPredictors()));
-        LogisticRegressionClassifier classifier = new LogisticRegressionClassifier(2);
+        LogisticRegressionClassifier classifier = new LogisticRegressionClassifier(dataset.numClasses);
         classifier.fit(dataset);
         Map<String, Double> LogRegmetrics = classifier.evaluate(dataset, metrics);
         System.out.println(LogRegmetrics);
-        LDAClassifier lda = new LDAClassifier(2);
+        LDAClassifier lda = new LDAClassifier(dataset.numClasses);
         lda.fit(dataset);
         Map<String, Double> LDAMetrics = lda.evaluate(dataset, metrics);
         System.out.println(LDAMetrics);
 
-        QDAClassifier qda = new QDAClassifier(2);
+        QDAClassifier qda = new QDAClassifier(dataset.numClasses);
         qda.fit(dataset);
         Map<String, Double> QDAMetrics = qda.evaluate(dataset, metrics);
         System.out.println(QDAMetrics);
