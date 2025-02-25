c38b1becaf1ddc76c2f084f551545381cd0efafc
sadelcarpio <sadelcarpioa@gmail.com>
2025-02-24 22:14:35 -0500

    Uncommented

--- classification/src/Main.java
+++ classification/src/Main.java
@@ -1,24 +1,26 @@
 import data.Dataset;
 import metrics.Accuracy;
 import metrics.Metric;
+import models.discriminative.LogisticRegressionClassifier;
 import models.generative.LDAClassifier;
 import org.ejml.simple.SimpleMatrix;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         Dataset dataset = new Dataset("data/dummy_data_classification.csv");
-//        System.out.println(Arrays.deepToString(dataset.getLabels()));
-//        System.out.println(Arrays.deepToString(dataset.getPredictors()));
-//        LogisticRegressionClassifier classifier = new LogisticRegressionClassifier(3);
-//        classifier.fit(dataset);
-//        Map<String, Double> metrics = classifier.evaluate(dataset, new Metric[]{new Accuracy()});
-//        System.out.println(metrics);
+        System.out.println("Labels: " + Arrays.deepToString(dataset.getLabels()));
+        System.out.println("Predictors: " + Arrays.deepToString(dataset.getPredictors()));
+        LogisticRegressionClassifier classifier = new LogisticRegressionClassifier(3);
+        classifier.fit(dataset);
+        Map<String, Double> LogRegmetrics = classifier.evaluate(dataset, new Metric[]{new Accuracy()});
+        System.out.println(LogRegmetrics);
         LDAClassifier priorClf = new LDAClassifier(3);
         priorClf.fit(dataset);
         priorClf.predict(new SimpleMatrix(dataset.getPredictors()));
-        Map<String, Double> metrics = priorClf.evaluate(dataset, new Metric[]{new Accuracy()});
-        System.out.println(metrics);
+        Map<String, Double> LDAMetrics = priorClf.evaluate(dataset, new Metric[]{new Accuracy()});
+        System.out.println(LDAMetrics);
     }
 }
