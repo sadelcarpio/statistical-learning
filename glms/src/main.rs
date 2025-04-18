2f17d87eee230e0efbef35aabb579e0bbd13da9b
sadelcarpio <sadelcarpioa@gmail.com>
2025-04-17 21:20:24 -0500

    Enter to exit program

--- glms/src/main.rs
+++ glms/src/main.rs
@@ -1,11 +1,15 @@
 mod optimizer;
 mod poisson_glm;
 
+use std::error::Error;
+use std::string::String;
+use std::io;
+use std::io::Write;
 use crate::optimizer::GradientDescent;
 use crate::poisson_glm::PoissonRegressor;
 use polars::prelude::*;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     let df: DataFrame = CsvReadOptions::default()
         .try_into_reader_with_file_path(Some("data/dummy_data.csv".into()))
         .unwrap()
@@ -24,4 +28,9 @@ fn main() {
     let metrics = model.evaluate(&predictors, &response);
     println!("Metrics: {:?}", metrics);
     println!("Trained params: {:?}", model.params.unwrap());
+    println!("\nPress Enter to exit the program.");
+    io::stdout().flush()?;
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    Ok(())
 }
